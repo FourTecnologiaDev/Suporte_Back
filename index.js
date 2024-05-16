738fccb02fe382431a14fdf167d7d5a18405c20a
Pedro TortoriellO <pedrooofreitas@gmail.com>
2024-05-16 12:31:10 -0400

    16/05

--- index.js
+++ index.js
@@ -14,26 +14,23 @@ mongoSchemaLogin();
 mongoSchemaTickets();
 mongoSchemaFinalizados();
 
-app.use(express.static("src"));
-app.use(cors({
-  origin: '*', // Permitir acesso de qualquer origem
-  methods: 'GET,PUT,POST,DELETE', // Métodos permitidos
-  allowedHeaders: 'Content-Type,Authorization', // Cabeçalhos permitidos
-}));
 
-mongoose.connect(process.env.MONGODB_URI, {
+app.use(express.static('src'));
+app.use(cors()); 
+
+  mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  useUnifiedTopology: true
+});  
 
 // routes
 app.use("/", routes);
 
 // Server
-const porta = 3000;
+const porta = 3000
 app.listen(process.env.PORT || porta);
 // app.listen(porta, function() {
-
-console.log("servidor rodando na porta: " + process.env.PORT || porta);
-
+ 
+   console.log("servidor rodando na porta: " + process.env.PORT || porta);
+   
 // });
